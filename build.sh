3405a216857be71ec07420b686612218360b77c6
kotp2001 <dubrovinkosta456@gmail.com>
2026-05-14 10:09:32 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.12
+apt-get update && apt-get install -y python3.12 python3.12-venv
+python3.12 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
